4b77eca058699d6a20257a95b3ede95e9e16efe9
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-01-11 18:16:33 +0900

    라우팅 완료

---
+++ src/components/SearchHeader.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SearchHeader() {
+  return <div>헤더</div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Videos from "./pages/Videos";
+import Detail from "./pages/Detail";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+/**
+ * 헤더를 항상 보여준다.
+ * 메인 페이지 : "/" or "/videos"
+ * 검색 후 페이지 : "/videos/:keyword"
+ * 디테일 페이지 : "/videos/watch/:videoId"
+ */
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Videos />,
+      },
+      {
+        path: "/videos",
+        element: <Videos />,
+      },
+      {
+        path: "/videos/:keyword",
+        element: <Videos />,
+      },
+      {
+        path: "/videos/watch/:videoId",
+        element: <Detail />,
+      },
+    ],
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
