8c46d192fac9fbe0820d73d2d5a3a453bf4fa009
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-12-15 06:28:46 +0900

    Feat : 무한스크롤 기능 완전한 완성 #4
    모든 문제 해결후 실제 데이터에 적용까지 완료 했습니다.

--- src/pages/SearchVideos.jsx
+++ src/pages/SearchVideos.jsx
@@ -9,31 +9,15 @@ import VideoCard from "../components/common/videos/VideoCard";
 
 export default function SearchVideos() {
   const { keyword } = useParams();
-  // const [videoData, setVideoData] = useState(null);
   const [videoList, setVideoList] = useState([]);
   const [nextPageToken, setNextPageToken] = useState(null);
-  const [render, setRender] = useState(false);
-  const [requireFetch, setRequireFetch] = useState(false);
-
-  // useEffect(() => {
-  //   fetchData(keyword, nextPageToken).then((data) => {
-  //     setVideoList(data.videos);
-  //     setNextPageToken(data.nextPageToken);
-  //   });
-  // }, []);
-
-  // useEffect(() => {
-  //   fetchData(keyword, nextPageToken).then((data) => {
-  //     setVideoList(data.videos);
-  //     setNextPageToken(data.nextPageToken);
-  //   });
-  // }, []);
+  const [requireFetch, setRequireFetch] = useState(true);
 
   useEffect(() => {
-    fetchData().then((data) => {
-      setVideoList((prev) => prev.concat(data.videos));
-      setNextPageToken(data.nextPageToken);
-    });
+    if (requireFetch) {
+      fetchData();
+      setRequireFetch(false);
+    }
   }, [requireFetch]);
 
   function handleInfinityScroll() {
@@ -42,20 +26,19 @@ export default function SearchVideos() {
     const scrollTop = document.documentElement.scrollTop;
 
     if ((scrollTop + clientHeight) / scrollHeight >= 0.9) {
-      console.log(nextPageToken);
-      fetchData(keyword, nextPageToken).then((data) => {
-        console.log(keyword, nextPageToken);
-        console.log(data);
-      });
-      // setRequireFetch((prev) => !prev);
+      setRequireFetch(true);
     }
   }
 
   async function fetchData() {
-    // const data = await fetchSearchData(keyword, nextPageToken);
-    const data = await fetchMocData(keyword, nextPageToken);
-    return data;
-    setVideoList((prevVideoList) => prevVideoList.concat(data.videos));
+    const data = await fetchSearchData(keyword, nextPageToken);
+    setVideoList((prevVideoList) => {
+      // 영상이 중복되는 경우가 있습니다.
+      // 중복되는 영상을 제거하는 코드입니다.
+      let jsonVideoList = prevVideoList.concat(data.videos).map(JSON.stringify);
+      let uniqueVideoList = new Set(jsonVideoList);
+      return [...uniqueVideoList].map(JSON.parse);
+    });
     setNextPageToken(data.nextPageToken);
   }
 
@@ -69,8 +52,6 @@ export default function SearchVideos() {
 
   return (
     <div>
-      <button onClick={() => setRender((prev) => !prev)}>강제렌더링</button>
-      <div className="fixed bold bg-red-500">{nextPageToken}</div>
       {videoList && (
         <ul className="grid gap-4 max-w-screen-2xl grid-cols-1 m-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 ">
           {videoList.map((video) => (
@@ -81,6 +62,42 @@ export default function SearchVideos() {
     </div>
   );
 }
+
+/**
+ * 스크롤 이벤트가 너무 많이 실행되는 것을 막아주는 함수
+ * @param {*} func
+ * @param {*} delay
+ * @returns
+ */
+function debounce(func, delay) {
+  let timerId;
+
+  return function (...args) {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+
+const fetchSearchData = async (keyword, nextPageToken) => {
+  const key = process.env.REACT_APP_YOUTUBE_API_KEY;
+  const SEARCH_RESULT_COUNT = 20;
+  const url = nextPageToken
+    ? `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${SEARCH_RESULT_COUNT}&q=${keyword}&pageToken=${nextPageToken}&key=${key}`
+    : `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${SEARCH_RESULT_COUNT}&q=${keyword}&key=${key}`;
+  const { data } = await axios.get(url);
+
+  let videos = data.items
+    .map((item) => {
+      item.id = item.id.videoId;
+      return item;
+    })
+    .filter((item) => item.id !== undefined); // id 가 undefined인 것들로 인해 key props 에러가 발생합니다. 이 동영상들은 제외 합니다.
+
+  return { videos, nextPageToken: data.nextPageToken };
+};
 
 const fetchMocData = async (keyword, nextPageToken) => {
   console.log(`fetchMocData 에서 사용된 token : ${nextPageToken}`);
@@ -111,44 +128,4 @@ const fetchMocData = async (keyword, nextPageToken) => {
   let result = { videos, nextPageToken: data.nextPageToken };
 
   return result;
-};
-
-/**
- * 스크롤 이벤트가 너무 많이 실행되는 것을 막아주는 함수
- * @param {*} func
- * @param {*} delay
- * @returns
- */
-function debounce(func, delay) {
-  let timerId;
-
-  return function (...args) {
-    clearTimeout(timerId);
-
-    timerId = setTimeout(() => {
-      func.apply(this, args);
-    }, delay);
-  };
-}
-
-const fetchSearchData = async (keyword, nextPageToken) => {
-  const key = process.env.REACT_APP_YOUTUBE_API_KEY;
-  const SEARCH_RESULT_COUNT = 20;
-  const url = nextPageToken
-    ? `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${SEARCH_RESULT_COUNT}&q=${keyword}&pageToken=${nextPageToken}&key=${key}`
-    : `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${SEARCH_RESULT_COUNT}&q=${keyword}&key=${key}`;
-  const { data } = await axios.get(url);
-  console.log(data);
-  console.log(url);
-  console.log(nextPageToken);
-  let newNextPageToken = data.nextPageToken;
-
-  let videos = data.items
-    .map((item) => {
-      item.id = item.id.videoId;
-      return item;
-    })
-    .filter((item) => item.id !== undefined); // id 가 undefined인 것들로 인해 key props 에러가 발생합니다. 이 동영상들은 제외 합니다.
-
-  return { videos, newNextPageToken };
 };
