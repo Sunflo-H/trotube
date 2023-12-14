faf514ca5add4228680bb29974ebb4239c1847a6
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2023-12-14 23:44:43 +0900

    Fix : 무한스크롤 기능 수정#4
    setNextPageToken이 작동하지 않는 문제 수정
    하지만 아직 근본적인 원인을 알지 못함
    원인 발견후 적용할 것

--- src/pages/SearchVideos.jsx
+++ src/pages/SearchVideos.jsx
@@ -8,63 +8,69 @@ import VideoCard from "../components/common/videos/VideoCard";
 // import { fetchSearchData } from "../utils/infinityScroll";
 
 export default function SearchVideos() {
-  const [videoList, setVideoList] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
   const { keyword } = useParams();
+  // const [videoData, setVideoData] = useState(null);
+  const [videoList, setVideoList] = useState([]);
   const [nextPageToken, setNextPageToken] = useState(null);
+  const [render, setRender] = useState(false);
+  const [requireFetch, setRequireFetch] = useState(false);
+
+  // useEffect(() => {
+  //   fetchData(keyword, nextPageToken).then((data) => {
+  //     setVideoList(data.videos);
+  //     setNextPageToken(data.nextPageToken);
+  //   });
+  // }, []);
+
+  // useEffect(() => {
+  //   fetchData(keyword, nextPageToken).then((data) => {
+  //     setVideoList(data.videos);
+  //     setNextPageToken(data.nextPageToken);
+  //   });
+  // }, []);
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData().then((data) => {
+      setVideoList((prev) => prev.concat(data.videos));
+      setNextPageToken(data.nextPageToken);
+    });
+  }, [requireFetch]);
 
-  useEffect(() => {
-    console.log("비디오 리스트 변경");
-    setIsLoading(true);
-    console.log(videoList);
-  }, [videoList]);
-
-  async function handleInfinityScroll() {
+  function handleInfinityScroll() {
     const clientHeight = document.documentElement.clientHeight;
     const scrollHeight = document.documentElement.scrollHeight;
     const scrollTop = document.documentElement.scrollTop;
 
-    if ((scrollTop + clientHeight) / scrollHeight >= 0.8) {
-      fetchData();
+    if ((scrollTop + clientHeight) / scrollHeight >= 0.9) {
+      console.log(nextPageToken);
+      fetchData(keyword, nextPageToken).then((data) => {
+        console.log(keyword, nextPageToken);
+        console.log(data);
+      });
+      // setRequireFetch((prev) => !prev);
     }
   }
 
   async function fetchData() {
-    console.log("fetch 실행");
-    const data = await fetchSearchData(keyword, nextPageToken);
-    // const data = await fetchMocData(keyword, nextPageToken);
-    setVideoList([...videoList, ...data.videos]);
-    setNextPageToken(data.newNextPageToken);
-  }
-
-  function debounce(func, delay) {
-    let timerId;
-
-    return function (...args) {
-      clearTimeout(timerId);
-
-      timerId = setTimeout(() => {
-        func.apply(this, args);
-      }, delay);
-    };
+    // const data = await fetchSearchData(keyword, nextPageToken);
+    const data = await fetchMocData(keyword, nextPageToken);
+    return data;
+    setVideoList((prevVideoList) => prevVideoList.concat(data.videos));
+    setNextPageToken(data.nextPageToken);
   }
 
   const debounceScroll = debounce(handleInfinityScroll, 300);
   useEffect(() => {
-    // window.addEventListener("scroll", handleInfinityScroll);
     window.addEventListener("scroll", debounceScroll);
     return () => {
-      // window.removeEventListener("scroll", handleInfinityScroll);
       window.removeEventListener("scroll", debounceScroll);
     };
   }, []);
 
   return (
     <div>
+      <button onClick={() => setRender((prev) => !prev)}>강제렌더링</button>
+      <div className="fixed bold bg-red-500">{nextPageToken}</div>
       {videoList && (
         <ul className="grid gap-4 max-w-screen-2xl grid-cols-1 m-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 ">
           {videoList.map((video) => (
@@ -76,15 +82,25 @@ export default function SearchVideos() {
   );
 }
 
-const fetchSearchData = async (keyword, nextPageToken) => {
-  const key = process.env.REACT_APP_YOUTUBE_API_KEY;
-  const SEARCH_RESULT_COUNT = 20;
-  const url = nextPageToken
-    ? `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${SEARCH_RESULT_COUNT}&q=${keyword}&pageToken=${nextPageToken}&key=${key}`
-    : `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${SEARCH_RESULT_COUNT}&q=${keyword}&key=${key}`;
-  const { data } = await axios.get(url);
-  let newNextPageToken = data.nextPageToken;
+const fetchMocData = async (keyword, nextPageToken) => {
+  console.log(`fetchMocData 에서 사용된 token : ${nextPageToken}`);
+  let url;
+  switch (nextPageToken) {
+    case "CBQQAA":
+      url = `/data/moc2.json`;
+      break;
+    case "CCgQAA":
+      url = `/data/moc3.json`;
+      break;
+    case "CDwQAA":
+      url = `/data/moc4.json`;
+      break;
+    default:
+      url = `/data/moc1.json`;
+      break;
+  }
 
+  const { data } = await axios.get(url);
   let videos = data.items
     .map((item) => {
       item.id = item.id.videoId;
@@ -92,13 +108,39 @@ const fetchSearchData = async (keyword, nextPageToken) => {
     })
     .filter((item) => item.id !== undefined); // id 가 undefined인 것들로 인해 key props 에러가 발생합니다. 이 동영상들은 제외 합니다.
 
-  return { videos, newNextPageToken };
+  let result = { videos, nextPageToken: data.nextPageToken };
+
+  return result;
 };
-const fetchMocData = async (keyword, nextPageToken) => {
+
+/**
+ * 스크롤 이벤트가 너무 많이 실행되는 것을 막아주는 함수
+ * @param {*} func
+ * @param {*} delay
+ * @returns
+ */
+function debounce(func, delay) {
+  let timerId;
+
+  return function (...args) {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+
+const fetchSearchData = async (keyword, nextPageToken) => {
   const key = process.env.REACT_APP_YOUTUBE_API_KEY;
   const SEARCH_RESULT_COUNT = 20;
-  const url = nextPageToken ? `/data/moc2.json` : `/data/moc1.json`;
+  const url = nextPageToken
+    ? `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${SEARCH_RESULT_COUNT}&q=${keyword}&pageToken=${nextPageToken}&key=${key}`
+    : `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${SEARCH_RESULT_COUNT}&q=${keyword}&key=${key}`;
   const { data } = await axios.get(url);
+  console.log(data);
+  console.log(url);
+  console.log(nextPageToken);
   let newNextPageToken = data.nextPageToken;
 
   let videos = data.items
